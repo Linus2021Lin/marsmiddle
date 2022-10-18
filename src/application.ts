1b16fc95866b54a222f96e3d60e2f22bb9666c3a
linuslin <linus2021_lin@edge-core.com>
2022-10-18 19:01:00 +0800

    [ Enhance >> RegExp ] Add RegExp service

--- src/application.ts
+++ src/application.ts
@@ -9,6 +9,7 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
+import {RegexpService} from './tools/regexp/regexp';
 
 export {ApplicationConfig};
 
@@ -40,5 +41,8 @@ export class MarsmiddleApplication extends BootMixin(
         nested: true,
       },
     };
+
+    // bind regexp
+    this.bind('regexpService').toClass(RegexpService);
   }
 }
