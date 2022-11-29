ff503f77bb25eb11dbffd680024dd4f7f0d938a4
linuslin <linus2021_lin@edge-core.com>
2022-11-29 17:59:30 +0800

    [ Enhance >> CustomHttpError ] Add property 'details' into CustomHttpError class to show more info of  error messages

--- src/tools/customError/customHttpError.ts
+++ src/tools/customError/customHttpError.ts
@@ -1,8 +1,12 @@
-export class CustomHttpError extends Error {
-  statusCode: number
+// See https://github.com/loopbackio/strong-error-handler
+// only can modify specified properties, such as 'statusCode', 'details'(for 4xx error staus code), 'errorCode', 'name', 'code'.
 
-  constructor(status: number, message: string) {
-    super(message)
-    this.statusCode = status
+export class CustomHttpError extends Error {
+  statusCode: number;
+  details?: string[];
+  constructor(status: number, message: string, ...details: string[]) {
+    super(message);
+    this.statusCode = status;
+    if (details.length != 0) { this.details = [...details]; }
   }
 }
