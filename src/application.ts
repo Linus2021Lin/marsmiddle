4ce4759e4211eb47ae23f260c50e18d7dd71f426
linuslin <linus2021_lin@edge-core.com>
2022-12-23 14:32:22 +0800

    [ Adjust >> Database ] Auto create database JSON when app starts & adjust gitignore

--- src/application.ts
+++ src/application.ts
@@ -30,6 +30,38 @@ export class MarsmiddleApplication extends BootMixin(
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+ 
+    //  Set up DB
+    var fs = require('fs');
+    const path = './database/memoryDB.json'
+    try {
+      if (fs.existsSync(path)) {
+        //file exists
+        console.log('Database Exists.');
+      } else {
+        //file doesn't exist
+        const initDbContent = {
+          "ids": {
+            "User": 2,
+            "UserCredentials": 2
+          },
+          "models": {
+            "User": {
+              "admin": "{\"id\":\"admin\",\"username\":\"admin\",\"role\":\"administrator\",\"tokens\":[\"defaultBlank\"]}"
+            },
+            "UserCredentials": {
+              "37e78818-1562-4b44-90c5-f3c6c1db98dd": "{\"id\":\"37e78818-1562-4b44-90c5-f3c6c1db98dd\",\"password\":\"$2a$10$Tk50yE46D/IkqXL8/CCs2OIfGjz7SMCGuQmjzKEOuBm04fZpusb9.\",\"userId\":\"admin\"}"
+            }
+          } 
+        };
+        fs.writeFile(path, JSON.stringify(initDbContent), function (err: any) {
+          if (err) throw err;
+          console.log('Database Initialized.');
+        });
+      }
+    } catch(err) {
+      console.error(err)
+    }
 
     // Set up the custom sequence
     this.sequence(MySequence);
